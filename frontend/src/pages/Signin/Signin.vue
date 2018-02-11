b74d9d141ad697917a2420a1ec4e062bc7f3620a
martinyounghoonkim <hoons0131@gmail.com>
2018-02-11 23:11:37 +0900

    Add router for entry files

--- frontend/src/pages/Signin/Signin.vue
+++ frontend/src/pages/Signin/Signin.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    test
-  </div>
+    <div>
+        test123
+    </div>
 </template>
 <script>
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,10 +7,10 @@ Vue.use(Router);
 export default new Router({
     mode: 'history',
     routes: [{
-        path: '/',
+        path: '/home',
         name: 'Signin',
         components: {
-            Signin,
+            contents: Signin,
         },
     }],
 });
