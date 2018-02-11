b0d7078d2fbb2c33811bcd9954015af00160f471
martinyounghoonkim <hoons0131@gmail.com>
2018-02-11 23:05:55 +0900

    Add eslint rules files

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,25 +2,51 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  extends: [
+    'standard',
+    'plugin:vue/essential'
+  ],
   parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2017,
     sourceType: 'module'
   },
   env: {
     browser: true,
   },
-  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-  extends: 'standard',
-  // required to lint *.vue files
   plugins: [
+    'html',
+    'standard',
     'vue'
   ],
   // add your custom rules here
   rules: {
-    // allow async-await
     'generator-star-spacing': 'off',
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'semi': [ 'error', 'always' ]
+    'indent': [ 'error', 4 ],
+    'brace-style': [ 'error', '1tbs' ],
+    'semi': [ 'error', 'always' ],
+    'no-console': 'error',
+    'comma-dangle': [ 'error', {
+      'arrays': 'never',
+      'objects': 'always-multiline',
+      'imports': 'never',
+      'exports': 'never',
+      'functions': 'never'
+    }],
+    'no-multiple-empty-lines': [ 'error', { 'max': 2, 'maxBOF': 1 }],
+    'no-undef': 'error',
+    'space-in-parens': ['error', 'never'],
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'space-before-function-paren': [
+      'error',
+      'always'
+    ],
+    'quotes': ['error', 'single'],
+    'space-before-blocks': [
+      'error',
+      'always'
+    ],
+    'no-empty': 'error',
+    'no-duplicate-imports': 'error'
   }
 }
